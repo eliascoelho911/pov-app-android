903c28ce7b0a57e28a64b5fd0596ce9370bf1fa2
Elias <elias_coelho911@hotmail.com>
2025-11-13 21:44:48 -0300

    Inatiliza o botão voltar e transforma o app em um launcher

--- app/src/main/java/com/eliascoelho911/pov_tests/MainActivity.kt
+++ app/src/main/java/com/eliascoelho911/pov_tests/MainActivity.kt
@@ -2,13 +2,22 @@ package com.eliascoelho911.pov_tests
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.eliascoelho911.pov_tests.ui.theme.PovtestsTheme
@@ -18,11 +27,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            BackHandler {
+                // do nothing
+            }
             PovtestsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .padding(innerPadding)
                     )
                 }
             }
@@ -32,10 +45,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface {
+        Column {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
